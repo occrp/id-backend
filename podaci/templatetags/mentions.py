bda8f9e9a47f71d1695edeec6d74aa068220f92a
Smári McCarthy <smari@mailpile.is>
2015-02-24 16:08:50 +0100

    r for regex

--- podaci/templatetags/mentions.py
+++ podaci/templatetags/mentions.py
@@ -2,7 +2,7 @@ from django_jinja import library
 import jinja2
 import re
 
-mention = re.compile("@\[([^@]*)\]\(([\w\d]+):([A-Za-z0-9_-]+)\)")
+mention = re.compile(r"@\[([^@]*)\]\(([\w\d]+):([A-Za-z0-9_-]+)\)")
 
 @library.global_function
 def parse_mentions(value):
