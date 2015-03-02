75834fdb516898c3e85aec7bc16471c3f5ef67a4
James Robb <james@gearedinteractive.ca>
2015-03-02 11:29:58 +0100

    first stage of settings seperation for django project

---
+++ settings/settings_local.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret! Change for your local setup!
+SECRET_KEY = ''
+
+ALLOWED_HOSTS = ["127.0.0.1:8000"]
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASE_ENGINE = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+
+NEO4J_HOST = ''
+NEO4J_PORT = ''
+NEO4J_ENDPOINT = ''
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-us'
+DATE_FORMAT = 'd-F-Y'
