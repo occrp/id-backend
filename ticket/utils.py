004128794c47f3eb23d8e9f5d940b211fcfb2fa7
James Robb <james@gearedinteractive.ca>
2015-03-06 16:30:07 +0100

    ticket list view

---
+++ ticket/utils.py
@@ -0,0 +1,35 @@
+from ticket import constants
+
+def split_open_tickets(tickets):
+    o = []
+    c = []
+    if tickets and len(tickets) > 0:
+        for t in tickets:
+            (o if t.status in constants.OPEN_TICKET_STATUSES else c).append(t)
+    return o, c
+
+def smart_truncate(content, length):
+    '''
+    truncate a string, ending on a word break and adding ellipses
+    '''
+    if len(content) <= length:
+        return content
+    else:
+        if ' ' in content[-20:]:
+            return content[:length].rsplit(' ', 1)[0] + '...'
+        else:  #if there is no word break, just do an ugly break
+            return content[:length-3] + '...'
+
+def get_actual_ticket(self):
+    try:
+        return self.personticket
+    except:
+        pass
+    try:
+        return self.companyticket
+    except:
+        pass
+    try:
+        return self.otherticket
+    except:
+        pass
