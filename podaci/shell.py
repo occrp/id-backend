06966886e2a66d7872b7e22d140ead787ff55478
Smári McCarthy <smari@mailpile.is>
2015-03-06 18:16:46 +0100

    More shell commands and Podaci features

--- podaci/shell.py
+++ podaci/shell.py
@@ -8,6 +8,7 @@
 import os
 import sys
 import cmd
+import glob
 from datetime import datetime
 import dateutil.parser
 from argparse import ArgumentParser
@@ -16,6 +17,22 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
 from settings.settings import *
 from podaci.filesystem import *
+from django.contrib.auth.models import User
+
+def getuser(u):
+    if not u.isdigit():
+        try:
+            user = User.objects.get(username=u)
+        except:
+            print "Error: User %s does not exist" % u
+            return None
+    else:
+        try:
+            user = User.objects.get(id=u)
+        except:
+            print "Error: User %s does not exist" % u
+            return None
+    return user
 
 class Strawman:
     def __init__(self, id, username):
@@ -54,7 +71,7 @@ class PodaciShell(cmd.Cmd):
 
         tag = params[0]
         count, files = self.fs.list_files(tag)
-        if "-l" in params:
+        if details:
             print "%-30s %-20s %-12s %s" % ("Name", "ID", "Added", "Pub User Writ Note Files")
             for f in files:
                 date = dateutil.parser.parse(f["date_added"])
@@ -64,6 +81,128 @@ class PodaciShell(cmd.Cmd):
                 print f.meta["name"]
 
         print "%d files" % count
+
+    def do_add(self, line):
+        params = line.split(" ")
+        for f in params:
+            if not os.path.isfile(f):
+                print "%s does not exist" % f
+
+        for f in params:
+            fid, fi, s = self.fs.create_from_file(f)
+            if s:
+                print "%s" % fid
+            else:
+                print "Could not create file %s." % f
+
+    def complete_add(self, text, line, begidx, endidx):
+        params = line.split(" ")
+        return glob.glob(params[-1]+'*')
+
+    def do_tag(self, line):
+        params = line.split(" ")
+        fileids = []
+        addtags = []
+        rmtags = []
+        for p in params:
+            if p[0] == "@":
+                fileids.append(p[1:])
+            elif p[0] == "+":
+                addtags.append(p[1:])
+            elif p[0] == "-":
+                rmtags.append(p[1:])
+            else:
+                print "Error: Parameters must start with @, + or -."
+                self.help_tag()
+                return
+
+        # FIXME: Verify that the tags exist.
+        for f in fileids:
+            fh = self.fs.get_file_by_id(f)
+
+            for t in addtags:
+                fh.add_tag(t)
+
+            for t in rmtags:
+                fh.remove_tag(t)
+
+    def help_tag(self):
+        print "Usage: tag @file1 @file2 @file3 +tag1 +tag2 -tag3"
+        print "This will add tags tag1 and tag2 to the three files, and remove tag3"
+
+    def do_perm(self, line):
+        params = line.split(" ")
+        fileids = []
+        addusers = []
+        rmusers = []
+        make_public = False
+        make_private = False
+        give_write = False
+
+        for p in params:
+            if p == "--write":
+                give_write = True
+                continue
+            if p == "--public":
+                make_public = True
+                continue
+            if p == "--private":
+                make_private = True
+                continue
+
+            if p[0] == "@":
+                fileids.append(p[1:])
+            elif p[0] == "+":
+                u = getuser(p[1:])
+                if u:
+                    addusers.append(u)
+                else:
+                    return
+            elif p[0] == "-":
+                u = getuser(p[1:])
+                if u:
+                    rmusers.append(u)
+                else:
+                    return
+            else:
+                print "Error: Parameters must start with @, + or -."
+                self.help_perm()
+                return
+
+        if make_public and make_private:
+            print "Error: You can't both make files public and private!"
+            return
+
+        for f in fileids:
+            fh = self.fs.get_file_by_id(f)
+            for u in addusers:
+                fh.add_user(u, give_write)
+
+            for u in rmusers:
+                fh.remove_user(u)
+
+            if make_public:
+                fh.make_public()
+            elif make_private:
+                fh.make_private()
+
+    def do_log(self, line):
+        params = line.split(" ")
+        fileids = []
+        for p in params:
+            if p[0] == "@":
+                fh = self.fs.get_file_by_id(p[1:])
+                print "=== Log for %s ===" % p[1:]
+                for line in fh["changelog"]:
+                    print "[%s] %s :: %s" % (line["date"], line["user"], line["message"])
+
+
+    def help_perm(self):
+        print "Usage: tag [-w] @file1 @file2 @file3 +user1 +user2 -user3"
+        print "This will allow users user1 and user2 to access the three files, and disallow user3"
+        print "  --write    Gives them write permission"
+        print "  --public   Makes the files public"
+        print "  --private  Makes the files not public"
 
     def do_exit(self, line):
         return True
