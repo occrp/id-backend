10519f130cd8ac2891b1f44707e015120b52a473
James Robb <james@gearedinteractive.ca>
2015-03-16 17:24:25 +0100

    reopen, close, cancel, and corresponding ticket updates implimented

--- ticket/mixins.py
+++ ticket/mixins.py
@@ -1,6 +1,27 @@
 from django.contrib import messages
 from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import JsonResponse
 from django.utils.translation import ugettext_lazy as _
+
+class TicketAjaxResponseMixin(object):
+
+    def form_invalid(self, form):
+        response = super(TicketAjaxResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        response = super(TicketAjaxResponseMixin, self).form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'message': unicode(_("Ticket successfully updated.")),
+                'pk': self.object.pk
+            }
+            return JsonResponse(data)
+        else:
+            return response
 
 class TicketUpdateMixin(object):
 
@@ -9,13 +30,11 @@ class TicketUpdateMixin(object):
         return super(TicketUpdateMixin, self).form_valid(form)
 
     def form_invalid(self, form):
-        messages.success(self.request, '')
         return super(TicketUpdateMixin, self).form_valid(form)
 
     def get_success_url(self):
         ticket = self.get_object()
         return reverse_lazy('ticket_details', kwargs={'ticket_id': ticket.id})
-
 
 class TicketCreateMixin(object):
 
