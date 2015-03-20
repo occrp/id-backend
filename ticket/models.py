305f39c83b00cf53b3febac049be659c6dfa02ac
Smári McCarthy <smari@occrp.org>
2015-03-20 19:09:24 +0100

    Allow blank reconciliation date

--- ticket/models.py
+++ ticket/models.py
@@ -431,7 +431,7 @@ class TicketCharge(models.Model, DisplayMixin):
 
     # whether the bill has been reconciled or not
     reconciled = models.BooleanField(default=False)
-    reconciled_date = models.DateTimeField()
+    reconciled_date = models.DateTimeField(blank=True)
     paid_status = models.CharField(max_length=70, choices=PAID_STATUS)
 
     # when the charge was created
