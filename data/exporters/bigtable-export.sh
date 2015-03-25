d95ce766a6b35628eb24cba4cbad5bd25676a355
Michał 'rysiek' Woźniak <rysiek@fwioo.pl>
2015-03-25 10:51:40 +0100

    bigtable_export dev/prod mixup fix; help info added to every error; I think we're done here

--- data/exporters/bigtable-export.sh
+++ data/exporters/bigtable-export.sh
@@ -5,38 +5,47 @@
 # Usage:
 # ./bigtable-export.sh (--prod|--dev) <path-to-old-investigative-dashboard-source> <path-to-google-app-engine-sdk> <output-directory>
 
-if [ -z "$1" ]; then
-  echo 'Please identify the source environment (either `--prod`, or `--dev`) as the first argument.'
-  exit 1
-elif [[ "$1" == "--prod" ]]; then
-  ID_APP="s~investigative-dashboard-occrp"
-  ID_APP_URL="https://investigative-dashboard-occrp.appspot.com"
-elif [[ "$1" == "--dev" ]]; then
-  ID_APP="s~investigative-dashboard-prod"
-  ID_APP_URL="https://investigative-dashboard-prod.appspot.com"
-elif [[ "$1" == "--help" ]]; then
+function be_help() {
   echo
   echo "Usage:"
   echo "  ./bigtable-export.sh (--prod|--dev) <path-to-old-investigative-dashboard-source> <path-to-google-app-engine-sdk> <output-directory>"
   echo
+}
+
+if [ -z "$1" ]; then
+  echo 'Please identify the source environment (either `--prod`, or `--dev`) as the first argument.'
+  be_help
+  exit 1
+elif [[ "$1" == "--dev" ]]; then
+  ID_APP="s~investigative-dashboard-occrp"
+  ID_APP_URL="https://investigative-dashboard-occrp.appspot.com"
+elif [[ "$1" == "--prod" ]]; then
+  ID_APP="s~investigative-dashboard-prod"
+  ID_APP_URL="https://investigative-dashboard-prod.appspot.com"
+elif [[ "$1" == "--help" ]]; then
+  be_help
   exit 0
 else
   echo "First argument is expected to be --prod, --dev, or --help"
+  be_help
   exit 2
 fi
 
 if [ -z "$2" ]; then
   echo "Please supply the path to the sources of the old version of investigative dashboard as the second argument."
+  be_help
   exit 3
 fi
 
 if [ -z "$3" ]; then
   echo "Please supply the path to Google App Engine SDK as the third argument."
+  be_help
   exit 4
 fi
 
 if [ -z "$4" ]; then
   echo "Please supply the path to the output directory as the fourth argument."
+  be_help
   exit 5
 fi
 
