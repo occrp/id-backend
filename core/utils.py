40976edfb0a08fb38690dad5c4ebcfd145e19eee
Michał 'rysiek' Woźniak <rysiek@fwioo.pl>
2015-03-27 14:33:29 +0100

    Revert ".profile removed (user.profile is the same thing as user these days), with:"
    This reverts commit a3f7c7bf9155d60ff7cc949c7f112d39c73c5151.

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,6 @@ def convert_group_to_select2field_choices(group):
     result = []
 
     for i in group:
-        result.append((i.id, ig.display_name))
+        result.append((i.id, i.profile.display_name))
 
     return result
