cc1dec996620463644050c2b5a5b99a0cd78c7f4
Michał 'rysiek' Woźniak <rysiek@fwioo.pl>
2015-03-30 16:40:57 +0200

    working on ticket import, not finished yet

--- data/importers/Tickets_to_ID2Tickets.py
+++ data/importers/Tickets_to_ID2Tickets.py
@@ -13,9 +13,13 @@ from ast import literal_eval
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 sys.path.append(os.path.abspath("../../"))
 from ticket.models import *
+from django.contrib.auth import get_user_model;
 
+# we're gonna need that...
+import re
 
 def convert(in_file):
+ 
     f = open(in_file, 'r')
     reader = csv.reader(f)
 
@@ -51,7 +55,7 @@ def convert(in_file):
                     value = 'other'
                 else:
                     raise ValueError('%s is not a valid ticket type' % value)
-            # that one's tricky, but luckily literal_eval comes to resque
+            # that one's tricky, but luckily literal_eval comes to rescue
             elif key == "family":
                 if value == '':
                     value = []
@@ -82,6 +86,9 @@ def convert(in_file):
     print "Setting up django..."
     django.setup()
 
+    # we need that regex for the 'responders' field
+    r = re.compile("u'UserProfile', (\d+)L, _app")
+
     i = 0
     for ticket in tickets:
         i += 1
@@ -95,12 +102,55 @@ def convert(in_file):
         elif ticket['ticket_type'] == 'other':
             t = OtherTicket()
         
+        responders = ''
+        volunteers = ''
         for key, value in ticket.iteritems():
+            print '+-- working on: %24s' % key
+            #sys.stdout.flush()
+            # this has to be a Profile instance
+            # or, actually, anything that we use as the User model these days
+            if key == "requester":
+                try:
+                    value = get_user_model().objects.get(old_google_key=value)
+                except:
+                    print('User with old_google_key: "%s" does not seem to exist; have you imported user data already?' % value)
+                    sys.exit(1)
+            # we don't need time here
+            elif key == 'deadline':
+                value = value.strip()[:10]
+            elif key == "responders":
+                # we're not going to save that into the ticket directly
+                # instead, we're saving it for use after the ticket is saved
+                responders = value
+                continue
+            elif key == "volunteers":
+                # we're not going to save that into the ticket directly
+                # instead, we're saving it for use after the ticket is saved
+                volunteers = value
+                continue
+            # set the attribute
             setattr(t, key, value)
+        
+        # databasing the database
         try:
+            # save the ticket
             t.save()
+            # handle responders...
+            print('Responders: "%s"' % responders)
+            for resp in r.finditer(responders):
+                print '+-- %s' % resp.group(1)
+                u = get_user_model().objects.get(old_google_key=resp.group(1))
+                t.responders.add(u)
+            # ...and volunteers
+            print('Volunteers: "%s"' % volunteers)
+            for vol in r.finditer(volunteers):
+                print '+-- %s' % vol.group(1)
+                u = get_user_model().objects.get(old_google_key=vol.group(1))
+                t.volunteers.add(u)
+        # wat.
         except IntegrityError, e:
             print "Skipping dupe: %s" % (e)
+      
     print "\rAdding tickets: Done."
 
 
