a525800c574b704ff90cb1ba0254c8044bc256d0
Smári McCarthy <smari@occrp.org>
2015-04-10 17:16:16 +0200

    Switching to UserTestCase

--- ticket/tests.py
+++ ticket/tests.py
@@ -1,37 +1,11 @@
-from django.contrib.auth.models import AnonymousUser#, User
-from django.contrib.auth import get_user_model # as per https://docs.djangoproject.com/en/dev/topics/auth/customizing/#referencing-the-user-model
 from django.test import TestCase
+from core.tests import UserTestCase
 from core.testclient import TestClient
 from settings.settings import *
 from django.core.urlresolvers import reverse
 from ticket.models import *
 
-class TicketsTest(TestCase):
-    def setUp(self):
-        self.anonymous_user = AnonymousUser()
-        self.normal_user = get_user_model().objects.create_user(
-            email='testuser@occrp.org', password='top_secret')
-        self.normal_user.is_user = True
-        self.normal_user.save()
-
-        self.volunteer_user = get_user_model().objects.create_user(
-            email='testvolunteer@occrp.org', password='top_secret')
-        self.volunteer_user.is_volunteer = True
-        self.volunteer_user.save()
-
-        self.staff_user = get_user_model().objects.create_user(
-            email='teststaff@occrp.org', password='top_secret')
-        self.staff_user.is_staff = True
-        self.staff_user.save()
-
-        self.admin_user = get_user_model().objects.create_user(
-            email='testsuperuser@occrp.org', password='top_secret')
-        self.admin_user.is_superuser = True
-        self.admin_user.save()
-
-    def tearDown(self):
-        pass
-
+class TicketsTest(UserTestCase):
     def test_create_person_ticket(self):
         client = TestClient()
         client.login_user(self.normal_user)
