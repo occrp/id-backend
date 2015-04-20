705348e86e0fcc619ccc47954cc346fb8ae57b44
Smári McCarthy <smari@occrp.org>
2015-04-20 14:52:28 +0200

    VKontakte search works

--- search/views.py
+++ search/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import TemplateView, UpdateView, FormView
 
-from search.api import ImageSearch, ImageSearchResult
+from datetime import datetime
+from search.api import ImageSearchDispatcher, ImageSearchResult
 
 
 class ImageSearch(TemplateView):
-    template_name = "search/imagesearch.html"
+    template_name = "search/search_images.jinja"
 
     def get_context_data(self):
-        s = ImageSearch()
-        results = s.search(lat, lon, radius, startdate, enddate)
+        q = self.request.GET.get("q", None).encode("utf-8")
+        if q:
+            lat = self.request.GET.get("lat")
+            lon = self.request.GET.get("lon")
+            radius = int(self.request.GET.get("radius", 5000))
+            startdate = self.request.GET.get("startdate", None)
+            enddate = self.request.GET.get("enddate", None)
+            offset = self.request.GET.get("offset", 0)
+            count = self.request.GET.get("count", 100)
+            s = ImageSearchDispatcher()
+            results = s.search(q, lat, lon, radius, startdate, enddate, offset, count)
+            return {"results": results}
 
+        return {"results": []}
