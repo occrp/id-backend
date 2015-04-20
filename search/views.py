4734f332423512d37db6686ffa91f158ddddceff
Smári McCarthy <smari@occrp.org>
2015-04-20 11:51:07 +0200

    New search app, for migrating old search functionality and adding new functionality

---
+++ search/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import TemplateView, UpdateView, FormView
+
+from search.api import ImageSearch, ImageSearchResult
+
+
+class ImageSearch(TemplateView):
+	template_name = "search/imagesearch.html"
+
+	def get_context_data(self):
+		pass
+
