f7a910872f21b45f1a3bf8567b63a373d802c002
Smári McCarthy <smari@occrp.org>
2015-04-21 12:40:57 +0200

    Require logged in users for image search

--- search/urls.py
+++ search/urls.py
@@ -9,9 +9,9 @@ import search.views
 urlpatterns = patterns('',
     url(r'^$',               perm('any', oldsearch.CombinedSearchHandler), name='search'),
     url(r'^entities/$',      perm('any', oldsearch.CombinedSearchHandler), name='search_entities'), # still needed for ajax only
-    url(r'^images/$',        perm('any', TemplateView, template_name="search/search_images.jinja"), name='search_images'),
-    url(r'^images/query/$',  perm('any', search.views.ImageSearchQuery), name='search_images_query'),
-    url(r'^images/check/$',  perm('any', search.views.ImageSearchCheck), name='search_images_check'),
+    url(r'^images/$',        perm('user', TemplateView, template_name="search/search_images.jinja"), name='search_images'),
+    url(r'^images/query/$',  perm('user', search.views.ImageSearchQuery), name='search_images_query'),
+    url(r'^images/check/$',  perm('user', search.views.ImageSearchCheck), name='search_images_check'),
 )
 
 
