be363d092937421ddbd78613d8589a7c9db25087
Smári McCarthy <smari@occrp.org>
2015-04-27 14:43:04 +0200

    Moved search views to search app and added statistics for search

--- search/urls.py
+++ search/urls.py
@@ -3,13 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from core.auth import perm
 
-from id import search as oldsearch
 import search.views
 
 urlpatterns = patterns('',
-    url(r'^$',               perm('any', oldsearch.CombinedSearchHandler), name='search'),
+    url(r'^$',               perm('any',  search.views.CombinedSearchHandler), name='search'),
     url(r'^results/$',       perm('user', search.views.SearchCheck), name='search_results'),
-    url(r'^entities/$',      perm('any', oldsearch.CombinedSearchHandler), name='search_entities'), # still needed for ajax only
+    url(r'^entities/$',      perm('any',  search.views.CombinedSearchHandler), name='search_entities'), # still needed for ajax only
 
     url(r'^images/$',        perm('user', search.views.ImageSearchTemplate, template_name="search/search_images.jinja"), name='search_images'),
     url(r'^images/query/$',  perm('user', search.views.SearchImageQuery), name='search_images_query'),
