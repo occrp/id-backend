11e00299fac5d6cfe5861990832640fd74a7a02a
James Robb <me@jamesrobb.ca>
2015-06-22 16:23:21 +0000

    cleanup in tests, add test for project delete, add helper function for projects, updates to urls for api calls

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,8 @@ from projects.views import dummy_view
 
 
 urlpatterns = patterns('',
-    url(r'^project_create/$',                        dummy_view, name='project_create'),
-    url(r'^project_list/$',                          dummy_view, name='project_list'),
+    url(r'^project/create/$',                        dummy_view, name='project_create'),
+    url(r'^project/list/$',                          dummy_view, name='project_list'),
+    url(r'^project/(?P<id>.+)/$',                    dummy_view, name='project_get'),
+    url(r'^project/(?P<id>.+)/delete/$',             dummy_view, name='project_delete')
 )
