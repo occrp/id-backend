0504b22da2502e1476a7a7dce0ffcb45a8e23b2b
James Robb <me@jamesrobb.ca>
2015-06-23 12:10:56 +0000

    test for adding users to a project

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^project/list/$',                          dummy_view, name='project_list'),
     url(r'^project/(?P<id>.+)/$',                    dummy_view, name='project_get'),
     url(r'^project/(?P<id>.+)/alter/$',                    dummy_view, name='project_alter'),
-    url(r'^project/(?P<id>.+)/delete/$',             dummy_view, name='project_delete')
+    url(r'^project/(?P<id>.+)/delete/$',             dummy_view, name='project_delete'),
+    url(r'^project/(?P<id>.+)/add_users/$',             dummy_view, name='project_add_users')
 )
