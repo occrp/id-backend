09ed45f55e7bd938ffff59894536fb52a396812a
James Robb <me@jamesrobb.ca>
2015-06-23 14:58:47 +0000

    assertions now fail properly rather than python attribute errors being triggered, a much better simluation!

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
+import simplejson
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def dummy_view(request, id=0):
-    html = ""
-    return HttpResponse(html)
+    json = {'id': 0, 'title': 'dummy_view_title', 'coordinator': 0, 'users': []}
+    return Response(json)
