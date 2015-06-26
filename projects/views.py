37508bc8d266c59df165bebfdf99ff5ab73a7244
James Robb <me@jamesrobb.ca>
2015-06-26 14:43:46 +0000

    basic api call project list and view

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,37 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 
+from rest_framework import generics
+from rest_framework import mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework.reverse import reverse
+
+from projects.models import Project
+from projects.serializers import ProjectSerializer
 
 import simplejson
+
+# -- API ROOT
+# -- general entry point for the api
+#
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'projects': reverse('project_list', request=request, format=format)
+    })
+
+class ProjectList(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ProjectDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def dummy_view(request, id=0):
