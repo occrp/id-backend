a70a009fd187e15b7f45751dff4acf467464077a
Smári McCarthy <smari@occrp.org>
2015-07-01 15:35:39 +0200

    Many updates on Projects

--- static/js/id2/projects.js
+++ static/js/id2/projects.js
@@ -1,17 +1,110 @@
 var ID2 = ID2 || {};
-ID2.Projects = angular.module('ID2Projects', ['ngMaterial']);
-ID2.Projects.config(function($interpolateProvider) {
-  $interpolateProvider.startSymbol('[[');
-  $interpolateProvider.endSymbol(']]');
-});
 
-ID2.Projects.controller('ID2ProjectsController', ['$scope', '$mdSidenav', '$mdDialog', '$http', function($scope, $mdSidenav, $mdDialog, $http) {
-    $scope.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
+ID2.ng = angular.module('ID2', ['ngRoute', 'ID2.Projects']).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/projects'});
+    }]);
+
+
+ID2.Projects = angular.module('ID2.Projects', ['ngMaterial']).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/projects', {
+                templateUrl: '/static/html/id2/project.list.html',
+                controller: 'ID2.Projects.list'
+            }).
+            when('/projects/:projectId', {
+                templateUrl: '/static/html/id2/project.view.html',
+                controller: 'ID2.Projects.view'
+            });
+    }]);
+
+function DialogController($scope, $mdDialog) {
+    $scope.cancel = function() {
+        $mdDialog.cancel();
     };
+    $scope.create = function(data) {
+        $mdDialog.hide(data);
+    };
+}
+
+ID2.Projects.controller('ID2.Projects.view', ['$scope', '$http', '$location', '$routeParams', '$mdDialog', function($scope, $http, $location, $routeParams, $mdDialog) {
+    $scope.project = { users: []};
+    $scope.storyList = [
+        {
+            "title": "The Missing Millions",
+            "thesis": "Millions of dollars that went missing after a truck took a wrong turn and got ambushed by radioactive vampires.",
+            "versions": "43",
+            "authors": [
+                {"name": "Dave Bloss", "email": "dave@occrp.org"},
+                {"name": "Miranda Patrucic", "email": "miranda@occrp.org"},
+            ],
+        },
+        {
+            "title": "Rustlers on the Moor",
+            "thesis": "A bunch of horses went missing, but later turned up in a meatball stew served at a glue factory.",
+            "versions": "2",
+            "authors": [
+                {"name": "Jody McPhillips", "email": "dave@occrp.org"},
+                {"name": "Paul Radu", "email": "miranda@occrp.org"},
+                {"name": "Lejla Camdzic", "email": "miranda@occrp.org"},
+                {"name": "Matt Sarnecki", "email": "miranda@occrp.org"},
+            ],
+        },
+        {
+            "title": "Deep-one Trafficking at All Time High",
+            "thesis": "Cthulu's spawn are being traded on open markets in the East, creating demand which is being satisfied by cross-border smuggling of these aquatic demigods.",
+            "versions": "2",
+            "authors": [
+                {"name": "Paul Radu", "email": "miranda@occrp.org"},
+                {"name": "Smári McCarthy", "email": "miranda@occrp.org"},
+                {"name": "Eleanor Rose", "email": "miranda@occrp.org"},
+            ],
+        }
+    ];
+
+    $scope.getProject = function(id) {
+        $http.get('/api/projects/' + id + "/").
+            success(function(data, status, headers, config) {
+                $scope.project = data;
+            }).
+            error(function(data, status, headers, config) {
+                // log error
+            });
+    }
+
+    $scope.storyListUpdate = function() {
+        $http.get('/api/projects/').
+            success(function(data, status, headers, config) {
+                $scope.storyList = data;
+            }).
+            error(function(data, status, headers, config) {
+                // log error
+            });
+    }
+
+    $scope.getUsers = function(query) {
+        var retval = {};
+        $http.get('/api/users/', {"q": query}).
+            success(function(data, status, headers, config) {
+                retval = data;
+            }).
+            error(function(data, status, headers, config) {
+
+            });
+        return retval;
+    }
+
+    $scope.getProject($routeParams.projectId);
+}]);
+
+ID2.Projects.controller('ID2.Projects.list', ['$scope', '$http', '$location', '$mdDialog', function($scope, $http, $location, $mdDialog) {
+    $scope.setLocation = function(view) {
+        $location.path(view);
+    }
 
     $scope.selectProject = function(item) {
-        console.log("going to " + item);
+        $location.path("/projects/" + item.id);
     }
 
     $scope.newProject = function(ev) {
@@ -52,14 +145,4 @@ ID2.Projects.controller('ID2ProjectsController', ['$scope', '$mdSidenav', '$mdDi
     $http.defaults.xsrfCookieName = 'csrftoken';
     $scope.projectList = []
     $scope.projectListUpdate();
-
-    function DialogController($scope, $mdDialog) {
-        $scope.cancel = function() {
-            $mdDialog.cancel();
-        };
-        $scope.create = function(data) {
-            $mdDialog.hide(data);
-        };
-    }
-
 }]);
