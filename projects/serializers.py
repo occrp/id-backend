0909dc6411e9f1d699ac1338321333d87543fe76
James Robb <me@jamesrobb.ca>
2015-07-01 14:35:53 +0000

    make users an optional argument for project post

--- projects/serializers.py
+++ projects/serializers.py
@@ -46,7 +46,7 @@ class UserField(serializers.RelatedField):
 class ProjectSerializer(serializers.ModelSerializer):
     description = serializers.CharField(max_length=250, allow_null=True, allow_blank=True, required=False)
     coordinator = UserField(queryset=get_user_model().objects.all())
-    users = UserField(many=True, queryset=get_user_model().objects.all())
+    users = UserField(many=True, queryset=get_user_model().objects.all(), required=False)
     story_count = serializers.IntegerField(source='stories.count', read_only=True)
 
     class Meta:
