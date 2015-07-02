9ba27f47a775c9d6843b696eee20e658454d67e6
Smári McCarthy <smari@occrp.org>
2015-07-02 15:43:46 +0200

    Beginnings of stories view

--- static/js/id2/projects.js
+++ static/js/id2/projects.js
@@ -16,6 +16,10 @@ ID2.Projects = angular.module('ID2.Projects', ['ngMaterial']).
             when('/projects/:projectId', {
                 templateUrl: '/static/html/id2/project.view.html',
                 controller: 'ID2.Projects.view'
+            }).
+            when('/stories/:storyId', {
+                templateUrl: '/static/html/id2/story.view.html',
+                controller: 'ID2.Stories.view'
             });
     }]);
 
@@ -27,6 +31,41 @@ function DialogController($scope, $mdDialog) {
         $mdDialog.hide(data);
     };
 }
+
+
+ID2.Projects.controller('ID2.Stories.view', ['$scope', '$http', '$location', '$routeParams', '$mdDialog', function($scope, $http, $location, $routeParams, $mdDialog) {
+    $scope.story = {};
+    $scope.storyVersion = {"text": ""};
+    $scope.saveStatus = 100;
+
+    $scope.textChanged = function() {
+        $scope.saveStatus = 0;
+    }
+
+    $scope.save = function() {
+        $scope.saveStatus = 0;
+        $http.put('/api/stories/' + $scope.story.id + "/", $scope.storyVersion).
+            success(function(data, status, headers, config) {
+                $scope.saveStatus = 100;
+            }).
+            error(function(data, status, headers, config) {
+
+            });
+    }
+
+    $scope.getStory = function(id) {
+        $http.get('/api/stories/' + id + "/").
+            success(function(data, status, headers, config) {
+                $scope.story = data;
+            }).
+            error(function(data, status, headers, config) {
+                // log error
+            });
+    }
+
+    $scope.getStory($routeParams.storyId);
+}]);
+
 
 ID2.Projects.controller('ID2.Projects.view', ['$scope', '$http', '$location', '$routeParams', '$mdDialog', function($scope, $http, $location, $routeParams, $mdDialog) {
     $scope.project = { users: []};
@@ -63,6 +102,10 @@ ID2.Projects.controller('ID2.Projects.view', ['$scope', '$http', '$location', '$
         }
     ];
 
+    $scope.openStory = function(item) {
+        $location.path("/stories/" + item.id);
+    }
+
     $scope.getProject = function(id) {
         $http.get('/api/projects/' + id + "/").
             success(function(data, status, headers, config) {
@@ -72,6 +115,33 @@ ID2.Projects.controller('ID2.Projects.view', ['$scope', '$http', '$location', '$
                 // log error
             });
     }
+
+    $scope.newStory = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/static/html/id2/story.new.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+        })
+        .then(function(data) {
+            console.log(data);
+            if (!data) { return; }
+            data["project"] = $scope.project.id;
+            console.log(data);
+            $http.post('/api/stories/', data).
+                success(function(data, status, headers, config) {
+                    console.log("Success:" + data);
+                    $scope.storyListUpdate();
+                    $scope.openStory(data.story.id);
+                }).
+                error(function(data, status, headers, config) {
+                    console.log("Error:" + data);
+                });
+            $scope.alert = data;
+        }, function() {
+            $scope.alert = 'You cancelled the dialog.';
+        });
+    };
 
     $scope.storyListUpdate = function() {
         $http.get('/api/projects/').
