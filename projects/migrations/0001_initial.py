a68a6fee999efcf0197dfdeba53bdd93cf99d0fb
James Robb <me@jamesrobb.ca>
2015-07-02 13:54:58 +0000

    clear migrations for projects and add new initial point

--- projects/migrations/0001_initial.py
+++ projects/migrations/0001_initial.py
@@ -17,8 +17,24 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=250)),
+                ('description', models.CharField(max_length=250, null=True, blank=True)),
                 ('coordinator', models.ForeignKey(related_name='coordinator', to=settings.AUTH_USER_MODEL)),
                 ('users', models.ManyToManyField(related_name='members', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ProjectPlan',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start_date', models.DateField()),
+                ('end_date', models.DateField()),
+                ('title', models.CharField(max_length=250)),
+                ('description', models.TextField()),
+                ('order', models.IntegerField()),
+                ('project', models.ForeignKey(to='projects.Project')),
             ],
             options={
             },
@@ -29,13 +45,14 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=250)),
-                ('published', models.DateField()),
+                ('thesis', models.CharField(max_length=500, null=True, blank=True)),
+                ('published', models.DateTimeField(null=True)),
                 ('podaci_root', models.CharField(max_length=50)),
                 ('artists', models.ManyToManyField(related_name='artists', to=settings.AUTH_USER_MODEL)),
                 ('copy_editors', models.ManyToManyField(related_name='copy_editors', to=settings.AUTH_USER_MODEL)),
                 ('editors', models.ManyToManyField(related_name='editors', to=settings.AUTH_USER_MODEL)),
                 ('fact_checkers', models.ManyToManyField(related_name='fact_checkers', to=settings.AUTH_USER_MODEL)),
-                ('project', models.ForeignKey(to='projects.Project')),
+                ('project', models.ForeignKey(related_name='stories', to='projects.Project')),
                 ('reporters', models.ManyToManyField(related_name='reporters', to=settings.AUTH_USER_MODEL)),
                 ('researchers', models.ManyToManyField(related_name='researchers', to=settings.AUTH_USER_MODEL)),
                 ('translators', models.ManyToManyField(related_name='translators', to=settings.AUTH_USER_MODEL)),
@@ -82,7 +99,7 @@ class Migration(migrations.Migration):
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('title', models.CharField(max_length=250)),
                 ('text', models.TextField()),
-                ('authored', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
                 ('story', models.ForeignKey(related_name='versions', to='projects.Story')),
             ],
             options={
@@ -93,6 +110,18 @@ class Migration(migrations.Migration):
             model_name='storytranslation',
             name='version',
             field=models.ForeignKey(to='projects.StoryVersion'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='projectplan',
+            name='related_stories',
+            field=models.ManyToManyField(to='projects.Story'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='projectplan',
+            name='responsible_users',
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL),
             preserve_default=True,
         ),
     ]
