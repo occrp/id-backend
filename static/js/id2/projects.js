82682e45523fd5aabc3817ddce73bbce57bef16c
Smári McCarthy <smari@occrp.org>
2015-07-03 09:45:46 +0200

    Blank list messages

--- static/js/id2/projects.js
+++ static/js/id2/projects.js
@@ -69,38 +69,7 @@ ID2.Projects.controller('ID2.Stories.view', ['$scope', '$http', '$location', '$r
 
 ID2.Projects.controller('ID2.Projects.view', ['$scope', '$http', '$location', '$routeParams', '$mdDialog', function($scope, $http, $location, $routeParams, $mdDialog) {
     $scope.project = { users: []};
-    $scope.storyList = [
-        {
-            "title": "The Missing Millions",
-            "thesis": "Millions of dollars that went missing after a truck took a wrong turn and got ambushed by radioactive vampires.",
-            "versions": "43",
-            "authors": [
-                {"name": "Dave Bloss", "email": "dave@occrp.org"},
-                {"name": "Miranda Patrucic", "email": "miranda@occrp.org"},
-            ],
-        },
-        {
-            "title": "Rustlers on the Moor",
-            "thesis": "A bunch of horses went missing, but later turned up in a meatball stew served at a glue factory.",
-            "versions": "2",
-            "authors": [
-                {"name": "Jody McPhillips", "email": "dave@occrp.org"},
-                {"name": "Paul Radu", "email": "miranda@occrp.org"},
-                {"name": "Lejla Camdzic", "email": "miranda@occrp.org"},
-                {"name": "Matt Sarnecki", "email": "miranda@occrp.org"},
-            ],
-        },
-        {
-            "title": "Deep-one Trafficking at All Time High",
-            "thesis": "Cthulu's spawn are being traded on open markets in the East, creating demand which is being satisfied by cross-border smuggling of these aquatic demigods.",
-            "versions": "2",
-            "authors": [
-                {"name": "Paul Radu", "email": "miranda@occrp.org"},
-                {"name": "Smári McCarthy", "email": "miranda@occrp.org"},
-                {"name": "Eleanor Rose", "email": "miranda@occrp.org"},
-            ],
-        }
-    ];
+    $scope.storyList = [];
 
     $scope.openStory = function(item) {
         $location.path("/stories/" + item.id);
@@ -128,7 +97,7 @@ ID2.Projects.controller('ID2.Projects.view', ['$scope', '$http', '$location', '$
             if (!data) { return; }
             data["project"] = $scope.project.id;
             console.log(data);
-            $http.post('/api/stories/', data).
+            $http.post('/api/story/', data).
                 success(function(data, status, headers, config) {
                     console.log("Success:" + data);
                     $scope.storyListUpdate();
