35ad9ed6246c4ae5b431022088efa20a5904a009
James Robb <me@jamesrobb.ca>
2015-07-06 17:53:43 +0000

    comment out empty tests

--- projects/tests.py
+++ projects/tests.py
@@ -1203,71 +1203,71 @@ class PipelineAPITest(APITestCase):
     #
     #
 
-    def test_assign_editor(self):
-        pass
+    # def test_assign_editor(self):
+    #     pass
 
-    def test_unassign_editor(self):
-        pass
+    # def test_unassign_editor(self):
+    #     pass
 
-    def test_assign_journalist(self):
-        pass
+    # def test_assign_journalist(self):
+    #     pass
 
-    def test_unassign_journalist(self):
-        pass
+    # def test_unassign_journalist(self):
+    #     pass
 
-    def test_assign_researcher(self):
-        pass
+    # def test_assign_researcher(self):
+    #     pass
 
-    def test_unassign_researcher(self):
-        pass
+    # def test_unassign_researcher(self):
+    #     pass
 
-    def test_assign_copyeditor(self):
-        pass
+    # def test_assign_copyeditor(self):
+    #     pass
 
-    def test_unassign_copyeditor(self):
-        pass
+    # def test_unassign_copyeditor(self):
+    #     pass
 
-    def test_assign_translator(self):
-        pass
+    # def test_assign_translator(self):
+    #     pass
 
-    def test_unassign_translator(self):
-        pass
+    # def test_unassign_translator(self):
+    #     pass
 
-    def test_assign_artist(self):
-        pass
+    # def test_assign_artist(self):
+    #     pass
 
-    def test_unassign_artist(self):
-        pass
+    # def test_unassign_artist(self):
+    #     pass
 
-    def test_set_story_status(self):
-        pass
+    # def test_set_story_status(self):
+    #     pass
 
-    def test_publish_story(self):
-        pass
+    # def test_publish_story(self):
+    #     pass
 
-    def test_unpublish_story(self):
-        pass
+    # def test_unpublish_story(self):
+    #     pass
 
-    def test_write_story(self):
-        pass
+    # def test_write_story(self):
+    #     pass
 
-    def test_add_art(self):
-        pass
+    # def test_add_art(self):
+    #     pass
 
-    def test_add_documents(self):
-        pass
+    # def test_add_documents(self):
+    #     pass
 
-    def test_prioritize_stories(self):
-        pass
+    # def test_prioritize_stories(self):
+    #     pass
 
-    def test_add_comment(self):
-        pass
+    # def test_add_comment(self):
+    #     pass
 
-    def test_edit_comment(self):
-        pass
+    # def test_edit_comment(self):
+    #     pass
 
-    def test_remove_comment(self):
-        pass
+    # def test_remove_comment(self):
+    #     pass
 
-    def test_lock_story(self):
-        pass
+    # def test_lock_story(self):
+    #     pass
