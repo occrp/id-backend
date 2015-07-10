95cf9d0cfe679131b82541c84d63150f32c9a708
James Robb <me@jamesrobb.ca>
2015-07-10 23:11:26 +0000

    update to models and accompanying migrations

--- projects/models.py
+++ projects/models.py
@@ -16,26 +16,27 @@ STORYSTATUSES = (
     (11, "Published"),
 )
 
-PROJECTUSERTYPES = (
-    (1, "Coordinator"),
-    (2, "User")
-)
+# PROJECTUSERTYPES = (
+#     (1, "Coordinator"),
+#     (2, "User")
+# )
 
-STORYUSERTYPES = (
-    (1, "Reporter"),
-    (2, "Researcher"),
-    (3, "Editor"),
-    (4, "Copy Editor"),
-    (5, "Fact Checker"),
-    (6, "Translator"),
-    (7, "Artist")
-)
+# STORYUSERTYPES = (
+#     (1, "Reporter"),
+#     (2, "Researcher"),
+#     (3, "Editor"),
+#     (4, "Copy Editor"),
+#     (5, "Fact Checker"),
+#     (6, "Translator"),
+#     (7, "Artist")
+# )
 
 class Project(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=250, blank=True, null=True)
-    users = models.ManyToManyField(AUTH_USER_MODEL, through="ProjectUser", related_name="members")
+    coordinators = models.ManyToManyField(AUTH_USER_MODEL, related_name="coordinators")
     # podaci_root = models.CharField(max_length=50)
+    users = models.ManyToManyField(AUTH_USER_MODEL, related_name="members")
 
     def hs_access(self, user):
         if user == self.coordinator:
@@ -43,12 +44,6 @@ class Project(models.Model):
         if user in self.users.all():
             return True
         return False
-
-class ProjectUser(models.Model):
-    project = models.ForeignKey(Project)
-    user = models.ForeignKey(AUTH_USER_MODEL)
-    user_type = models.IntegerField(choices=PROJECTUSERTYPES)
-
 
 class Story(models.Model):
     project = models.ForeignKey(Project, related_name='stories')
