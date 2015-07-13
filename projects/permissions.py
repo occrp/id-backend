57fc3409c95eea3081feded2f68c145ee17c2153
James Robb <me@jamesrobb.ca>
2015-07-13 15:21:39 +0000

    future import

--- projects/permissions.py
+++ projects/permissions.py
@@ -1,5 +1,10 @@
+from __future__ import absolute_import
+
 import rules
+
 from rest_framework import permissions
+
+import projects.rules
 
 class CanAlterDeleteProject(permissions.BasePermission):
 
