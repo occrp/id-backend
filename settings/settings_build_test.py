9b6ab6ed1b55a29ae19d35ce6989e15add1176e1
James Robb <me@jamesrobb.ca>
2015-07-17 15:35:32 +0000

    settings for test building reflects changes to settings_local example

--- settings/settings_build_test.py
+++ settings/settings_build_test.py
@@ -57,6 +57,11 @@ DEFAULTS = {
     },
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    'PAGE_SIZE': 10
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
