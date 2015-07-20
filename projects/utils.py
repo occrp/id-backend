a16283128a1af1af0fa5e18042a4a9fd0fe77c34
James Robb <me@jamesrobb.ca>
2015-07-20 12:08:51 +0000

    update to utilities for story versions filter

--- projects/utils.py
+++ projects/utils.py
@@ -21,3 +21,14 @@ def user_in_story_or_project_filter(story_objects, user):
                                     Q(artists__in=[user]) |
                                     Q(project__coordinators=user) |
                                     Q(project__users__in=[user])).distinct()
+
+def user_in_story_or_project_from_story_version_filter(story_version_objects, user):
+    return story_version_objects.filter(Q(story__reporters__in=[user]) |
+                                        Q(story__researchers__in=[user]) |
+                                        Q(story__editors__in=[user]) |
+                                        Q(story__copy_editors__in=[user]) |
+                                        Q(story__fact_checkers__in=[user]) |
+                                        Q(story__translators__in=[user]) |
+                                        Q(story__artists__in=[user]) |
+                                        Q(story__project__coordinators=user) |
+                                        Q(story__project__users__in=[user])).distinct()
