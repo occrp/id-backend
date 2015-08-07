989fc95b669173f040c8d841a91595a489a84bb3
Smári McCarthy <smari@occrp.org>
2015-08-07 16:18:13 +0200

    New URL structure

--- podaci/urls.py
+++ podaci/urls.py
@@ -1,38 +1,19 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
-from podaci import search, info, files, tags
-from core.auth import perm
+from podaci import views
 
 urlpatterns = patterns('',
-    url(r'^$',                             perm("staff", info.Home), name='podaci_info_home'),
-    url(r'^help/$',                        perm("staff", info.Help), name='podaci_info_help'),
-    url(r'^search/$',                      perm("staff", search.Search), name='podaci_search'),
-    url(r'^search/mention/$',              perm("staff", search.SearchMention), name='podaci_search_mention'),
+    url(r'^$', view.api_root),
+    url(r'^search/$', views.Search.as_view(), name='podaci_search'),
+    url(r'^file/$', views.FileList.as_view(), name='podaci_file_list'),
+    url(r'^file/(?P<id>[0-9]+)/$', views.FileDetail.as_view(), name='podaci_file_detail'),
+    url(r'^file/(?P<id>.+)/download/$', views.FileDownload.as_view(), name='podaci_file_download'),
 
-    url(r'^info/status/$',                 perm("admin", info.Status), name='podaci_info_status'),
-    url(r'^info/statistics/$',             perm("admin", info.Statistics), name='podaci_info_statistics'),
+    url(r'^tag/$', views.TagList.as_view(), name='podaci_tag_list'),
+    url(r'^tag/(?P<id>[0-9]+)/$', views.TagDetail.as_view(), name='podaci_tag_detail'),
 
-    url(r'^file/create/$',                 perm("staff", files.Create), name='podaci_files_create'),
-    url(r'^file/(?P<id>.+)/note/add/$',    perm("staff", files.NoteAdd), name='podaci_files_note_add'),
-    url(r'^file/(?P<id>.+)/note/delete/$', perm("staff", files.NoteDelete), name='podaci_files_note_add'),
-    url(r'^file/(?P<id>.+)/note/update/$', perm("staff", files.NoteUpdate), name='podaci_files_note_add'),
-    url(r'^file/(?P<id>.+)/delete/$',      perm("staff", files.Delete), name='podaci_files_delete'),
-    url(r'^file/(?P<id>.+)/update/$',      perm("staff", files.Update), name='podaci_files_upload'),
-    url(r'^file/(?P<id>.+)/download/$',    perm("staff", files.Download), name='podaci_files_download'),
-    url(r'^file/(?P<id>.+)/$',             perm("staff", files.Details), name='podaci_files_details'),
+    url(r'^collection/$', views.CollectionList.as_view(), name='podaci_collection_list'),
+    url(r'^collection/(?P<id>[0-9]+)/$', views.CollectionDetail.as_view(), name='podaci_collection_detail'),
 
-    url(r'^tag/create/$',                  perm("staff", tags.Create), name='podaci_tags_create'),
-    url(r'^tag/list/$',                    perm("staff", tags.List), name='podaci_tags_list'),
-    url(r'^tag/(?P<id>.+)/delete/$',       perm("staff", tags.Delete), name='podaci_tags_delete'),
-    url(r'^tag/(?P<id>.+)/update/$',       perm("staff", tags.Update), name='podaci_tags_upload'),
-
-    url(r'^tag/selection/overview/$',      perm("staff", tags.Overview), name='podaci_tags_overview'),
-    url(r'^tag/(?P<id>.+)/overview/$',     perm("staff", tags.Overview), name='podaci_tags_overview'),
-
-    url(r'^tag/selection/zip/$',           perm("staff", tags.Zip), name='podaci_tags_zip'),
-    url(r'^tag/(?P<id>.+)/zip/$',          perm("staff", tags.Zip), name='podaci_tags_zip'),
-    url(r'^tag/(?P<id>.+)/$',              perm("staff", tags.Details), name='podaci_tags_details'),
+    #url(r'^tag/selection/overview/$',      perm("staff", tags.Overview), name='podaci_tags_overview'),
+    # url(r'^tag/(?P<id>.+)/overview/$',     perm("staff", tags.Overview), name='podaci_tags_overview'),
 )
-
-
