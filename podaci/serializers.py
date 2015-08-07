1acaba4398db3e853fab1e481d29c550c7a07d32
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2015-08-07 18:24:15 +0200

    podaci serializers and views still a WIP

--- podaci/serializers.py
+++ podaci/serializers.py
@@ -2,6 +2,10 @@ from podaci.models import PodaciFile, PodaciTag, PodaciCollection
 from rest_framework import serializers
 
 class TagSerializer(serializers.ModelSerializer):
+    # we need files
+    # but only those that the user has access to
+    files = UserField(many=True, queryset=PodaciFile.objects.filter())
+    
     class Meta:
         model = PodaciTag
         fields = ('id', 'name', 'icon', 'files')
@@ -9,9 +13,12 @@ class TagSerializer(serializers.ModelSerializer):
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = PodaciFile
-        fields = ('id', 'name', 'title', 'date_added', 'created_by', 'filename', 'url', 'sha256', 'size', 'mimetype', 'description', 'tags', 'collections')
+        fields = ('id', 'name', 'title', 'date_added', 'created_by', 'filename',
+                  'url', 'sha256', 'size', 'mimetype', 'description', 'tags', 'collections')
 
 class CollectionSerializer(serializers.ModelSerializer):
+    # we need files
+    # but only those that the user has access to
     class Meta:
         model = PodaciCollection
         fields = ('id', 'name', 'description', 'owner', 'files')
