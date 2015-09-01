fff33bed13022179a15619d5efd8d0d8f7492a21
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-01 16:50:23 +0200

    Finally a working tag editor.

--- podaci/serializers.py
+++ podaci/serializers.py
@@ -1,4 +1,5 @@
 from podaci.models import PodaciFile, PodaciTag, PodaciCollection
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from id.models import Profile
 
@@ -19,11 +20,24 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'icon', 'files')
 
 
+class TagField(serializers.RelatedField):
+
+    def to_representation(self, tag):
+        return tag.name
+
+    def to_internal_value(self, name):
+        try:
+            return PodaciTag.objects.get(name=name)
+        except ObjectDoesNotExist:
+            t = PodaciTag.objects.create(name=name)
+            t.save()
+            return t
+
+
 class FileSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(allow_null=True)
-    tags = serializers.SlugRelatedField(many=True, read_only=False,
-                                        allow_empty=True, slug_field='name',
-                                        queryset=PodaciTag.objects.filter())
+    title = serializers.CharField(allow_null=True, allow_blank=True)
+    tags = TagField(many=True, read_only=False, allow_empty=True,
+                    queryset=PodaciTag.objects.filter())
 
     class Meta:
         model = PodaciFile
@@ -35,9 +49,14 @@ class FileSerializer(serializers.ModelSerializer):
         read_only_fields = ('sha256', 'url', 'filename', 'created_by',
                             'date_added', 'thumbnail')
 
-    # def update(self, instance, data):
-    #     instance.save()
-    #     return instance
+    def update(self, instance, data):
+        instance.title = data.get('title')
+        instance.description = data.get('description')
+        instance.tags.clear()
+        for tag in data.get('tags', []):
+            instance.tags.add(tag)
+        instance.save()
+        return instance
 
 
 class CollectionSerializer(serializers.ModelSerializer):
