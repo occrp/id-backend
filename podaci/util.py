9c6269143c426c23da7907f1a389c6b56845e570
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-02 11:39:32 +0200

    Clean up podaci model code a bit.

---
+++ podaci/util.py
@@ -0,0 +1,17 @@
+from uuid import uuid4
+from hashlib import sha256
+
+
+def sha256sum(filename, blocksize=65536):
+    f = None
+    if type(filename) in [str, unicode]:
+        f = open(filename, "rb")
+    else:
+        f = filename
+
+    hash = sha256()
+    for block in iter(lambda: f.read(blocksize), ""):
+        hash.update(block)
+
+    f.seek(0)
+    return hash.hexdigest()
