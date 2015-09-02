fd4b0f150225847b9cb9cd4e89a6d6b4dd9d39c5
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-02 14:40:38 +0200

    Fix up OpenCorporates search service.

--- search/searchers/opencorporates.py
+++ search/searchers/opencorporates.py
@@ -1,11 +1,41 @@
-from search.searchers.base import DocumentSearcher
+import urllib2
+import urllib
+import json
+
+from search.searchers.base import DocumentSearcher, ResultSet
+from search.searchers.base import DocumentSearchResult
+
+
+def reconcile_matches(corp):
+    url = 'http://opencorporates.com/reconcile?query=%s' % (
+        urllib.quote(corp))
+    data = json.load(urllib2.urlopen(url))
+    return(data['result'])
+
+
+def details_for_corp(corpid):
+    url = 'http://api.opencorporates.com/%s' % corpid
+    data = json.load(urllib2.urlopen(url))
+    if 'results' not in data or 'company' not in data['results']:
+        return None
+    return data['results']['company']
 
 
 class EntitySearchOpenCorporates(DocumentSearcher):
     PROVIDER = "OpenCorporates"
-    SEARCHER = opencorporates.OpenCorpSearch()
 
     def search(self, q, offset=0, limit=100, **kwargs):
-        results = self.SEARCHER.search(q, offset, limit)
-        self.result_count = results.resultcount
-        return results.resultdata
+        matches = reconcile_matches(q)
+        resultset = ResultSet(total=len(matches))
+        for match in matches[offset:offset+limit]:
+            company = details_for_corp(match['id'])
+            if company is None:
+                continue
+            result = DocumentSearchResult(self.PROVIDER,
+                                          company.get('opencorporates_url'),
+                                          company.get('updated_at'),
+                                          company.get('company_type'),
+                                          company.get('name'),
+                                          metadata=company)
+            resultset.append(result)
+        return resultset
