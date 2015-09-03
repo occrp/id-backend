dff3bbd14be5336cfd2c0513bff1b06465983e0d
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-03 10:35:23 +0200

    Clean up search code and remove unused components.

--- search/urls.py
+++ search/urls.py
@@ -6,14 +6,9 @@ from core.auth import perm
 import search.views
 
 urlpatterns = patterns('',
-    url(r'^old/$',           perm('any',  search.views.CombinedSearchHandler), name='search_old'),
-    # url(r'^q/$',           perm('any',  search.views.HandleQuery), name='query'),
-
     url(r'^results/$',       perm('any', search.views.SearchCheck), name='search_results'),
-    url(r'^document/$',      perm('any',  search.views.DocumentSearchTemplate), name='search'),
-    # url(r'^$',               perm('any',  search.views.DocumentSearchTemplate), name='search'),
+    url(r'^document/$',      perm('any', search.views.DocumentSearchTemplate), name='search'),
     url(r'^document/query/$',perm('any', search.views.DocumentSearchQuery), name='search_documents_query'),
-    url(r'^entity/$',        perm('any',  search.views.CombinedSearchHandler), name='search_entities'), # still needed for ajax only
 
     url(r'^image/$',         perm('user', search.views.ImageSearchTemplate), name='search_images'),
     url(r'^image/query/$',   perm('user', search.views.SearchImageQuery), name='search_images_query'),
