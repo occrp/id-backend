ac421c51eede35ce415eb7662758c5440ac75697
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-04 10:01:29 +0200

    Use requests to do HTTP in OC and searchers.

--- search/searchers/opencorporates.py
+++ search/searchers/opencorporates.py
@@ -1,47 +1,33 @@
-import urllib2
-import urllib
+import math
+import requests
 import logging
-import json
 
 from search.searchers.base import DocumentSearcher, ResultSet
 from search.searchers.base import DocumentSearchResult
 
 log = logging.getLogger(__name__)
 
-
-def reconcile_matches(corp):
-    url = 'https://opencorporates.com/reconcile?query=%s' % (
-        urllib.quote(corp))
-    try:
-        data = json.load(urllib2.urlopen(url))
-        return data['result']
-    except urllib2.HTTPError as err:
-        log.exception(err)
-        return []
-
-
-def details_for_corp(corpid):
-    url = 'https://api.opencorporates.com/%s' % corpid
-    try:
-        data = json.load(urllib2.urlopen(url))
-        if 'results' not in data or 'company' not in data['results']:
-            return None
-        return data['results']['company']
-    except urllib2.HTTPError as err:
-        log.exception(err)
-        return None
+SEARCH_URL = 'https://api.opencorporates.com/v0.4/companies/search'
 
 
 class EntitySearchOpenCorporates(DocumentSearcher):
     PROVIDER = "OpenCorporates"
 
-    def search(self, q, offset=0, limit=100, **kwargs):
-        matches = reconcile_matches(q)
-        resultset = ResultSet(total=len(matches))
-        for match in matches[offset:offset+limit]:
-            company = details_for_corp(match['id'])
-            if company is None:
-                continue
+    def search(self, q, offset=0, limit=20, **kwargs):
+        try:
+            res = requests.get(SEARCH_URL, params={
+                'q': q,
+                'per_page': limit,
+                'page': int(1 + math.ceil(offset / max(1, limit)))
+            })
+            results = res.json().get('results', {})
+        except Exception as e:
+            log.exception(e)
+            return ResultSet(total=0)
+
+        resultset = ResultSet(total=results.get('total_count'))
+        for match in results.get('companies', []):
+            company = match.get('company')
             result = DocumentSearchResult(self.PROVIDER,
                                           company.get('opencorporates_url'),
                                           company.get('updated_at'),
