f51260479a4cb612090811d553ff556eae40a344
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2015-09-15 15:50:11 +0200

    docker.sh ready for fastcgi; also fixed a minor bug there (fixtures now really load only when DEBUG is True)

--- docker.sh
+++ docker.sh
@@ -30,26 +30,44 @@ while ! python manage.py migrate --noinput; do
     echo      '#####################################################################'
 done
 
+echo -ne 'from django.conf import settings\nprint settings.DEBUG\n\n' | python manage.py shell 2>/dev/null | grep True > /dev/null 
+DEBUG=$?
+
 echo -e '\n#####################################################################'
-if echo -ne 'from django.conf import settings\nprint settings.DEBUG\n\n' | python manage.py shell 2>/dev/null | grep True > /dev/null; then
+# 0 is "true" in UNIX/bash world
+if [ $DEBUG -eq 0 ]; then
     echo      '# DEBUG is True, loading fixtures...'
+    python manage.py loaddata 'id/fixtures/initial_data.json'
+    
 else
     echo      '# DEBUG is False, omitting fixtures...'
 fi
 echo      '#####################################################################'
-
-python manage.py loaddata 'id/fixtures/initial_data.json'
         
 # get the IP address
 IP="$( ip -o -f inet a show dev eth0 | cut -d ' ' -f 7 | sed 's/\/.*//' )"
 echo -e '\n#####################################################################'
-echo      "# running django on $IP..."
+
+if [ $DEBUG -eq 0 ]; then
+    echo      "# running debug django (runserver) on $IP..."
+else
+    echo      "# running production django (runfcgi) on $IP..."
+fi
 echo      '#####################################################################'
 
 while true; do
-    python manage.py runserver 0.0.0.0:8000
+    if [ $DEBUG -eq 0 ]; then
+        python manage.py runserver 0.0.0.0:8000
+    else
+        python manage.py runfcgi method=prefork host=0.0.0.0 port=8000
+    fi
+    
     sleep 5
     echo -e '\n#####################################################################'
-    echo      "# restarting django on $IP..."
+    if [ $DEBUG -eq 0 ]; then
+        echo      "# restarting debug django (runserver) on $IP..."
+    else
+        echo      "# restarting production django (runfcgi) on $IP..."
+    fi
     echo      '#####################################################################'
 done
