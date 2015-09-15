f8bb341ec32e3e43ddf4ffcd9d20569d42e147a8
Smári McCarthy <smari@occrp.org>
2015-09-15 15:10:07 +0200

    Adding error 400

--- id/errors.py
+++ id/errors.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 
+def _400(request, **kwargs):
+	return return_error(400, request, **kwargs)
+
 def _401(request, **kwargs):
 	return return_error(401, request, **kwargs)
 
@@ -13,11 +16,11 @@ def _500(request, **kwargs):
 	return return_error(500, request, **kwargs)
 
 def return_error(code, request, **kwargs):
-    
+
     user = None
     try:
         user = request.user
     except AttributeError:
         pass
-    
+
     return render_to_response("errors/%s.jinja" % code, {"request": request, "user": user})
