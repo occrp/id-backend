aa4ba57f7d7d0f106c6fd70dd7853114744d7cbd
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2015-09-15 15:50:11 +0200

    docker.sh ready for fastcgi; also fixed a minor bug there (fixtures now really load only when DEBUG is True)

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,14 @@
 """
-WSGI config for id project.
+WSGI config for id2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "id.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
