c34898bd01a81d171d919e87b26224ce8529c665
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2015-09-15 14:47:58 +0200

    a bit of sanity in error handling

--- id/errors.py
+++ id/errors.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render_to_response
 
-def _400(request, **kwargs):
-	return render_to_response("errors/400.jinja", {"request": request, "user": request.user})
+def _401(request, **kwargs):
+	return return_error(401, request, **kwargs)
 
 def _403(request, **kwargs):
-	return render_to_response("errors/403.jinja", {"request": request, "user": request.user})
+	return return_error(403, request, **kwargs)
 
 def _404(request, **kwargs):
-	return render_to_response("errors/404.jinja", {"request": request, "user": request.user})
+	return return_error(404, request, **kwargs)
 
 def _500(request, **kwargs):
-	return render_to_response("errors/500.jinja", {"request": request, "user": request.user})
+	return return_error(500, request, **kwargs)
+
+def return_error(code, request, **kwargs):
+    
+    user = None
+    try:
+        user = request.user
+    except AttributeError:
+        pass
+    
+    return render_to_response("errors/%s.jinja" % code, {"request": request, "user": user})
