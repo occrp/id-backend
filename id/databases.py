d6576437be5a183b7807aaf36e65ca0a8dc2a911
Smári McCarthy <smari@occrp.org>
2015-10-26 16:13:29 +0100

    Some improvements to the database listings

--- id/databases.py
+++ id/databases.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from django.views.generic import *
+from id.countries import COUNTRIES
 from id.models import ExternalDatabase
 from id.forms import CountryFilterForm
 import re
@@ -10,20 +11,9 @@ class ExternalDatabaseList(ListView):
 
     badre = re.compile("\[\'(.*)\'\]")
 
-    def _fix_country(self, country):
-        # for some reason, googlebot cooks up broken urls like
-        # ?country=%5B'MD'%5D&_locale=en
-        # turn them into real ones
-        if self.badre.match(country):
-            country = self.badre.match(country).group(1)
-        return country
-
     def get_queryset(self):
-        print self.request.GET
         country = self.request.GET.get('country')
         if country:
-            print "Got country: %s" % country
-            country = self._fix_country(country)
             query = ExternalDatabase.objects.filter(country=country)
         else:
             query = ExternalDatabase.objects.all()
@@ -34,6 +24,7 @@ class ExternalDatabaseList(ListView):
         context = super(ExternalDatabaseList, self).get_context_data(**kwargs)
         context["filter_form"] = CountryFilterForm()
         context["count"] = ExternalDatabase.objects.count()
+        context["countries"] = dict(COUNTRIES)
         try:
             context["jurisdictions"] = (ExternalDatabase.objects.values("country")
                                         .annotate(total=Count('country'))
