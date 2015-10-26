5b3c0ba88ffd0e7de8e712908f7fc814463cf261
Friedrich Lindenberg <friedrich@pudo.org>
2015-10-26 18:04:31 +0100

    fix import.

--- ticket/constants.py
+++ ticket/constants.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
-from id.countries import COUNTRIES, CONTINENTS, REGIONS, CURRENCIES
+from core.countries import COUNTRIES
+
 
 # FIXME: This is stupid
 def get_choice(choice, choices, compare_index=None, return_index=None):
@@ -193,30 +194,3 @@ CIRCULATION_TYPES = (
     ('250k-500k', _('250,000 to 500,000')),
     ('500k+', _('More than 500,000')),
 )
-
-
-DATABASE_TYPES = (
-    ('business', _('Business Registry')),
-    ('regional', _('Regional procurement registry')),
-    ('regulatory', _('Regulatory Agency')),
-    ('commercial', _('Commercial Register')),
-    ('court', _('Court Registry')),
-    ('gazette', _('Gazette')),
-    ('land', _('Land Registry')),
-    ('ip', _('Intellectual Property')),
-    ('copyright', _('Copyright')),
-    ('land_tribunal', _('Land Tribunal')),
-    ('maritime', _('Maritime')),
-)
-
-
-DATABASE_COUNTRIES = (
-    ('', _('All Databases')),
-    ('GLOBAL', _('Global Databases')),
-    ('-', '---'),
-    ('AFRICA', _('Africa')),
-    ('ASIA', _('Asia')),
-    ('EUROPE', _('Europe')),
-    ('LAMERICA', _('Latin America')),
-    ('-', '---'),
-) + COUNTRIES[1:]
