9da3127bc3e767052c8decd11ae808e267917847
Friedrich Lindenberg <friedrich@pudo.org>
2015-10-27 10:30:52 +0100

    Move some data-related components to datavault.

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from core.models import Notification
 
+
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
