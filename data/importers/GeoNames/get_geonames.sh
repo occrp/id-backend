3ac0f38223a53b237836abfbab985d6abbb0a7e7
Friedrich Lindenberg <friedrich@pudo.org>
2015-10-27 10:30:52 +0100

    Move some data-related components to datavault.

--- data/importers/GeoNames/get_geonames.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget http://download.geonames.org/export/dump/allCountries.zip
-wget http://download.geonames.org/export/dump/countryInfo.txt
-unzip allCountries.zip
-mkdir output
-python gen_csvs.py
