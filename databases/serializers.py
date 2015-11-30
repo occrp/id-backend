5ba2586aab83afb9f755d4f2b3a4b69748061965
Smári McCarthy <smari@occrp.org>
2015-11-30 13:48:31 +0200

    Add external database serializer

---
+++ databases/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from databases.models import ExternalDatabase
+
+class DatabaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExternalDatabase
+        fields = ('id', 'agency', 'db_type', 'country', 'paid',
+                  'registration_required', 'government_db', 'url',
+                  'notes', 'blog_post', 'video_url')
