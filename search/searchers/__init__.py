f337e52aa4f5c45988f5cc527bec2efdc33cdcd4
Friedrich Lindenberg <friedrich@pudo.org>
2016-01-12 16:50:47 +0100

    Disable youtube searcher as key has been revoked.

--- search/searchers/__init__.py
+++ search/searchers/__init__.py
@@ -1,10 +1,10 @@
 from search.searchers.instagram import MediaSearchInstagram
 from search.searchers.vkontakte import MediaSearchVK
-from search.searchers.youtube import MediaSearchYouTube
+# from search.searchers.youtube import MediaSearchYouTube
 from search.searchers.podacisearcher import DocumentSearchPodaci
 from search.searchers.datatracker import DocumentSearchDataTracker
 from search.searchers.opencorporates import EntitySearchOpenCorporates
 
-SEARCHERS = [MediaSearchInstagram, MediaSearchVK, MediaSearchYouTube,
+SEARCHERS = [MediaSearchInstagram, MediaSearchVK,  # MediaSearchYouTube,
              DocumentSearchPodaci, EntitySearchOpenCorporates,
              DocumentSearchDataTracker]
