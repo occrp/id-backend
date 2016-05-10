336f5b5a52452ec69e97675d54522b46cbe19bcb
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-10 01:35:58 +0100

    Adding jQuery plugin to accurately serialize checkboxes.
    By default, SerializeArray() will leave out checkboxes
    that have the value 'false' ("off"). Because this function is
    here being used to create a JSON string, which is then sent
    to the REST API, these fields will be left out if not
    checked. This will create a problem, if a) something
    is being updated (via PUT), and b) checkbox is 'false' in form,
    and c) the field is already 'true' on server.
    
    This plugin solves this problem by accurately populating
    'false' checkboxes, along with 'true' ones.

--- static/js/id2/id2.database.js
+++ static/js/id2/id2.database.js
@@ -5,11 +5,62 @@ ID2.Database = {};
 ID2.Database.init = function() {
     $(".db-register-form").submit(ID2.Database.registerFormSubmitHandler);
     $(".database-items .database-item .db-delete-btn").click(ID2.Database.deleteClickHandler);
+    ID2.Database.jQueryPluginLoad();
 };
+
+/*
+ * Fancy jQuery plugin to accurately
+ * serialize checkboxes - i.e., to leave
+ * them in, even if they are not checked.
+ */
+
+
+ID2.Database.jQueryPluginLoad = function() {
+    (function ($) {
+         $.fn.serialize = function (options) {
+             return $.param(this.serializeArray(options));
+         };
+ 
+         $.fn.serializeArray = function (options) {
+             var o = $.extend({
+                 checkboxesAsBools: false
+             }, options || {});
+ 
+             var rselectTextarea = /select|textarea/i;
+             var rinput = /text|hidden|password|search|url/i;
+ 
+             return this.map(function () {
+                 return this.elements ? $.makeArray(this.elements) : this;
+             })
+            .filter(function () {
+                return this.name && !this.disabled &&
+                   (this.checked
+                   || (o.checkboxesAsBools && this.type === 'checkbox')
+                   || rselectTextarea.test(this.nodeName)
+                   || rinput.test(this.type));
+            })
+            .map(function (i, elem) {
+                 var val = $(this).val();
+                 return val == null ?
+                 null :
+                 $.isArray(val) ?
+                 $.map(val, function (val, i) {
+                    return { name: elem.name, value: val };
+                 }) :
+                 {
+                     name: elem.name,
+                     value: (o.checkboxesAsBools && this.type === 'checkbox') ? //moar ternaries!
+                           (this.checked ? 'true' : 'false') :
+                           val
+                 };
+             }).get();
+        };
+    })(jQuery);
+}
 
 
 ID2.Database.serializeForm = function($form){
-    var serialized = $form.serializeArray();
+    var serialized = $form.serializeArray({ checkboxesAsBools: true });
     var s = '';
     var data = {};
 
