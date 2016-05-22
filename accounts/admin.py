ac5fed60217ec02764a13ccd5ce48aa091d02ba1
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-22 19:06:43 +0100

    Moved Budgets and Statistics away from id/admin. Removed empty file.
    Moved Budgets manipulation form (admin) from id into tickets, as Budgets
    are part of ticket-app.
    
    Moved Statitics (admin) from id to accounts, as this is more
    related to accounts (although not hugely...)
    
    Removed empty file id/middlewere.
    
    id/admin.py gone by now.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
+from django.views.generic import TemplateView
 
-# Register your models here.
+from core.utils import version
+from ticket.models import Ticket, PersonTicket, CompanyTicket, OtherTicket, TicketCharge 
+from id.models import Network, Profile, AccountRequest
+
+class Statistics(TemplateView):
+    template_name = "admin/statistics.jinja"
+
+    def get_context_data(self):
+        return {
+            "version": version(),
+            "tickets_opened": Ticket.objects.count(),
+            "tickets_people": PersonTicket.objects.count(),
+            "tickets_company": CompanyTicket.objects.count(),
+            "tickets_other": OtherTicket.objects.count(),
+            "tickets_closed": Ticket.objects.filter(status="closed").count(),
+            "tickets_in_progress": Ticket.objects.filter(status="new").count(),
+            "accounts_approved": Profile.objects.filter(is_user=True).count(),
+            "accounts_user": Profile.objects.filter(is_user=True).count(),
+            "accounts_volunteer": Profile.objects.filter(is_volunteer=True).count(),
+            "accounts_staff": Profile.objects.filter(is_staff=True).count(),
+            "accounts_admin": Profile.objects.filter(is_superuser=True).count(),
+            "accreq_total": AccountRequest.objects.count(),
+            "accreq_approved": AccountRequest.objects.filter(approved=True).count(),
+            "accreq_rejected": AccountRequest.objects.filter(approved=False).count(),
+            "accreq_outstanding": AccountRequest.objects.filter(approved=None).count(),
+            "networks": Network.objects.all(),
+            "unaffiliated_costs_total": sum([x.cost for x in TicketCharge.objects.filter(user__network=0)]),
+            "unaffiliated_users_count": Profile.objects.filter(network=None).count()
+        }
+
+
