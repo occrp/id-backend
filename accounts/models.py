7e5123ac5cb58f8a67d1a08bcdfa3bbc483abde5
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-26 15:52:40 +0100

    Migrating model AccountRequest from id-app to accounts-app.
    Firstly, the model-code was moved from id/models.py to
    accounts/model.py
    
    Then, the data is moved by a series of migration scripts.
    The first one to run is id/migrations/0018_rename_accountrequests.py,
    which will begin by removing a unique-constraint. Then it simply
    renames the table "id_accountrequest" to "accounts_accountrequest".
    Then it creates a fake "id_accountrequest" table, which is torn
    down immediately (this is done to satisfy the migration-system,
    as it will attempt to remove a table that otherwise would not exist
    The migration-system will insist on creating such a migration, if
    it is not in place).
    
    Next is accounts/migrations/0001_initial.py (assured via dependency),
    which will make sure that accounts_accountrequest is in place. This
    should always be 'faked' by the migration-system, not touching
    the table.
    
    Next is accounts/migrations/0002_initial.py (also assured via dependency),
    which will recreate "accounts_accountrequest" with all data but implicitly
    removing all indexes, and then recreate the indexes. Removing the indexes
    will make sure that they do not have 'id_' as a prefix in their names.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,110 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from core.mixins import DisplayMixin
+from settings.settings import AUTH_USER_MODEL
+
+REQUEST_TYPES = (
+    ('requester', _('Information Requester')),
+    ('volunteer', _('Volunteer'))
+)
+
+
+######## Account management ############
+class AccountRequest(models.Model, DisplayMixin):
+    request_type = models.CharField(blank=False, max_length=64, choices=REQUEST_TYPES)
+    user = models.ForeignKey(AUTH_USER_MODEL, blank=False)
+    approved = models.NullBooleanField(default=None, blank=True, null=True, verbose_name=_('Approved'))
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        verbose_name=_('Date Created'))
+
+    # temporary fix for updated profiles
+    already_updated = models.BooleanField(default=False)
+
+    GROUPS = []
+    MAIL_TEMPLATE = 'accountrequest/mail_notification.jinja'
+
+    def __str__(self):
+        return "%s:%s:%s:%s" % (self.user, self.request_type, self.approved, self.date_created)
+
+    class Meta:
+        ordering = ['request_type', 'approved', 'date_created']
+        unique_together = (('user', 'request_type'),)
+
+    def get_display_value(self, property_name):
+        if property_name != 'approved':
+            return super(AccountRequest, self).get_display_value(property_name)
+        else:
+            return ('--' if self.approved is None
+                    else _('Yes') if self.approved is True
+                    else _('No'))
+
+    def approve(self):
+        """
+        Adds the Account Request's email address to the appropriate Google
+        groups, and marks the request as approved.
+        """
+        if self.request_type == 'volunteer':
+            self.user.is_volunteer = True
+            self.user.save()
+        elif self.request_type == 'request':
+            self.user.is_user = True
+            self.user.save()
+
+        self.approved = True
+        self.save()
+        self.notify_approved()
+
+    def reject(self):
+        """
+        Removes the user from the group!
+        """
+        if self.request_type == 'volunteer':
+            self.user.is_volunteer = False
+            self.user.save()
+        elif self.request_type == 'request':
+            self.user.is_user = False
+            self.user.save()
+
+        self.approved = False
+        self.save()
+        self.notify_rejected()
+
+    def notify_received(self):
+        self.email_notification(
+            to=self.user.email,
+            subject=unicode(_('Your Account Request was received')),
+            template='mail/account_request/received.jinja',
+            context={'request': self}
+        )
+        for admin in AUTH_USER_MODEL.objects.filter(is_superuser=True):
+            with templocale(admin.locale or 'en'):
+                self.email_notification(
+                    to=admin.email,
+                    subject=unicode(_('An Account Request was received')),
+                    template='mail/account_request/received_admin.jinja',
+                    context={'request': self}
+                )
+
+    def notify_approved(self):
+        self.email_notification(
+            to=self.user.email,
+            subject=unicode(_('An update to your Account Request')),
+            template='mail/account_request/approved.jinja',
+            context={'request': self}
+        )
+
+    def notify_rejected(self):
+        self.email_notification(
+            to=self.user.email,
+            subject=unicode(_('Your Account Request was rejected')),
+            template='mail/account_request/rejected.jinja',
+            context={'request': self}
+        )
+
+    def email_notification(self, to, subject, template, context):
+        pass
+
+
 
 
--- id/models.py
+++ id/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.core.mail import send_mail
 from django.utils import timezone
 
+from accounts.models import AccountRequest
 from core.mixins import DisplayMixin, NotificationMixin
 from core.models import notification_channel_format, channel_components
 from core.countries import COUNTRIES
@@ -21,12 +22,6 @@ REQUESTER_TYPES = (
     ('cost', _('Covering Cost')),
     ('cost_plus', _('Covering Cost +'))
 )
-
-REQUEST_TYPES = (
-    ('requester', _('Information Requester')),
-    ('volunteer', _('Volunteer'))
-)
-
 
 logger = logging.getLogger(__name__)
 
@@ -356,103 +351,5 @@ class Profile(AbstractBaseUser, NotificationMixin, PermissionsMixin):
         verbose_name_plural = _('users')
         abstract = False
         swappable = 'AUTH_USER_MODEL'
-
-
-######## Account management ############
-class AccountRequest(models.Model, DisplayMixin):
-    request_type = models.CharField(blank=False, max_length=64, choices=REQUEST_TYPES)
-    user = models.ForeignKey(AUTH_USER_MODEL, blank=False)
-    approved = models.NullBooleanField(default=None, blank=True, null=True, verbose_name=_('Approved'))
-    date_created = models.DateTimeField(auto_now_add=True,
-                                        verbose_name=_('Date Created'))
-
-    # temporary fix for updated profiles
-    already_updated = models.BooleanField(default=False)
-
-    GROUPS = []
-    MAIL_TEMPLATE = 'accountrequest/mail_notification.jinja'
-
-    def __str__(self):
-        return "%s:%s:%s:%s" % (self.user, self.request_type, self.approved, self.date_created)
-
-    class Meta:
-        ordering = ['request_type', 'approved', 'date_created']
-        unique_together = (('user', 'request_type'),)
-
-    def get_display_value(self, property_name):
-        if property_name != 'approved':
-            return super(AccountRequest, self).get_display_value(property_name)
-        else:
-            return ('--' if self.approved is None
-                    else _('Yes') if self.approved is True
-                    else _('No'))
-
-    def approve(self):
-        """
-        Adds the Account Request's email address to the appropriate Google
-        groups, and marks the request as approved.
-        """
-        if self.request_type == 'volunteer':
-            self.user.is_volunteer = True
-            self.user.save()
-        elif self.request_type == 'request':
-            self.user.is_user = True
-            self.user.save()
-
-        self.approved = True
-        self.save()
-        self.notify_approved()
-
-    def reject(self):
-        """
-        Removes the user from the group!
-        """
-        if self.request_type == 'volunteer':
-            self.user.is_volunteer = False
-            self.user.save()
-        elif self.request_type == 'request':
-            self.user.is_user = False
-            self.user.save()
-
-        self.approved = False
-        self.save()
-        self.notify_rejected()
-
-    def notify_received(self):
-        self.email_notification(
-            to=self.user.email,
-            subject=unicode(_('Your Account Request was received')),
-            template='mail/account_request/received.jinja',
-            context={'request': self}
-        )
-        for admin in AUTH_USER_MODEL.objects.filter(is_superuser=True):
-            with templocale(admin.locale or 'en'):
-                self.email_notification(
-                    to=admin.email,
-                    subject=unicode(_('An Account Request was received')),
-                    template='mail/account_request/received_admin.jinja',
-                    context={'request': self}
-                )
-
-    def notify_approved(self):
-        self.email_notification(
-            to=self.user.email,
-            subject=unicode(_('An update to your Account Request')),
-            template='mail/account_request/approved.jinja',
-            context={'request': self}
-        )
-
-    def notify_rejected(self):
-        self.email_notification(
-            to=self.user.email,
-            subject=unicode(_('Your Account Request was rejected')),
-            template='mail/account_request/rejected.jinja',
-            context={'request': self}
-        )
-
-    def email_notification(self, to, subject, template, context):
-        pass
-
-
 
 
