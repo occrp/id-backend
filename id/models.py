ea0b9e463fbe46e0c2a012d202d91a12883a610e
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-26 12:22:51 +0100

    Moving id.models.Feedback to its proper home, feedback.models.Feedback
    This involves:
      * Moving model definition to the new home
      * Changing all relevant imports to point to the new home
      * Migrate: Renaming 'id_feedback' table to 'feedback_feedback'
      * Migrate: Feedback-model creation takes place after renaming
      * Migrate: Creating fake id_feedback-table, and remove it immediately
                 so that migrations does not complain about it being missing
                 when removing it.

--- id/models.py
+++ id/models.py
@@ -454,17 +454,5 @@ class AccountRequest(models.Model, DisplayMixin):
         pass
 
 
-class Feedback(models.Model):
-    name = models.CharField(blank=False, max_length=100)
-    email = models.EmailField(blank=False)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    message = models.TextField()
 
-    def __unicode__(self):
-        return u"From: %s <%s>\nDate: %s\n\n%s\n" % (self.name, self.email,
-            self.timestamp, self.message)
 
-    #def save(self):
-    #    self.notify("Received feedback from %s." % (self.name), action="add")
-    #    super(Feedback, self).save()
-    #    # Send e-mail!
