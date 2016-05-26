0bdb334347efa92725791baeb665d6d93773b84e
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-26 12:22:51 +0100

    Moving id.models.Feedback to its proper home, feedback.models.Feedback
    This involves:
      * Moving model definition to the new home
      * Changing all relevant imports to point to the new home
      * Migrate: Renaming 'id_feedback' table to 'feedback_feedback'
      * Migrate: Feedback-model creation takes place after renaming
      * Migrate: Creating fake id_feedback-table, and remove it immediately
                 so that migrations does not complain about it being missing
                 when removing it.

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(blank=False, max_length=100)
+    email = models.EmailField(blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+    def __unicode__(self):
+        return u"From: %s <%s>\nDate: %s\n\n%s\n" % (self.name, self.email,
+            self.timestamp, self.message)
+
