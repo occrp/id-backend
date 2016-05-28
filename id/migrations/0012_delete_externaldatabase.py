90f0d8053255a7a081d23a5d6212b24a3d5599ad
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-28 13:10:45 +0100

    Revoke earlier change: This migration has already been applied.

--- id/migrations/0012_delete_externaldatabase.py
+++ id/migrations/0012_delete_externaldatabase.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-import django.db
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -11,13 +10,7 @@ class Migration(migrations.Migration):
 
     database_operations = [
         migrations.AlterModelTable('ExternalDatabase', 'databases_externaldatabase'),
-
-        # If running on PostgreSQL, rename the sequencer also - no sequencer left behind!
-        migrations.RunSQL(
-            "ALTER SEQUENCE id_externaldatabase_id_seq RENAME TO databases_externaldatabase_id_seq" if django.db.connection.vendor == "postgresql" \
-             else "SELECT 1"
-        )
-    ]
+   ]
 
     state_operations = [
         migrations.DeleteModel('ExternalDatabase')
@@ -26,5 +19,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.SeparateDatabaseAndState(
             database_operations=database_operations,
-            state_operations=state_operations)
+            state_operations=state_operations
+        ),
+
     ]
