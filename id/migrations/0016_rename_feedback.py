ff5fea053f0c927745848840b0d6dfd872e7aa5d
Gudmundur Haraldsson <gdh1@hi.is>
2016-05-28 13:15:38 +0100

    Now also renaming the Feedback-sequencer.
    Now we not only rename the underlying database table for
    Feedback, we also rename the sequencer PostgreSQL
    creates for the table. We do this, because Django might
    anticipate the sequencer to have a particular name when
    performing some migration at later stages in the future.

--- id/migrations/0016_rename_feedback.py
+++ id/migrations/0016_rename_feedback.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.db
 
 
 class Migration(migrations.Migration):
@@ -14,4 +15,10 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             "ALTER TABLE id_feedback RENAME TO feedback_feedback"
         ),
+        # If running on PostgreSQL, rename the sequencer also - no sequencer left behind!
+        migrations.RunSQL(
+            "ALTER SEQUENCE id_feedback_id_seq RENAME TO feedback_feedback_id_seq" if django.db.connection.vendor == "postgresql" \
+             else "SELECT 1"
+        ),
+
     ]
