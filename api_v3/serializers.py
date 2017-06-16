f90c1085b5ec3c2bd6633038064b1b0c8a401668
Stas SUȘCOV <stas@nerd.ro>
2017-06-16 17:52:11 +0100

    Properly handle polymorphic attributes.

--- api_v3/serializers.py
+++ api_v3/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework_json_api import serializers, relations
+from rest_framework import fields
 
 from .models import Profile, Ticket, Action, Attachment, Comment, Responder
 
@@ -79,16 +80,46 @@ class CommentSerializer(serializers.ModelSerializer):
         )
 
 
+class ActionRelatedField(relations.ResourceRelatedField):
+    """Custom polymorphic serializer for action types."""
+
+    def get_attribute(self, instance):
+        not_comment = (
+            self.field_name == 'comment' and
+            not isinstance(instance.action, Comment))
+        not_attachment = (
+            self.field_name == 'attachment' and
+            not isinstance(instance.action, Attachment))
+        not_responder_user = (
+            self.field_name == 'responder_user' and
+            not isinstance(instance.action, Profile))
+
+        if not_comment or not_attachment or not_responder_user:
+            raise fields.SkipField()
+
+        return super(ActionRelatedField, self).get_attribute(instance)
+
+
 class ActionSerializer(serializers.ModelSerializer):
 
-    actor = relations.ResourceRelatedField(read_only=True)
-    action = relations.ResourceRelatedField(read_only=True)
-    target = relations.ResourceRelatedField(read_only=True)
+    user = relations.ResourceRelatedField(read_only=True, source='actor')
+    ticket = relations.ResourceRelatedField(read_only=True, source='target')
+
+    comment = ActionRelatedField(read_only=True, source='action')
+    attachment = ActionRelatedField(read_only=True, source='action')
+    responder_user = ActionRelatedField(read_only=True, source='action')
 
     class Meta:
         model = Action
-        fields = ('actor', 'action', 'verb', 'target')
         resource_name = 'Activity'
+        fields = (
+            'user',
+            'verb',
+            'ticket',
+            'attachment',
+            'comment',
+            'responder_user'
+        )
 
 
 class ResponderSerializer(serializers.ModelSerializer):
