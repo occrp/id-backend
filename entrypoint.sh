b4eee03a64ee5cdf9d03078f47b3747736bcb7a8
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2017-07-13 15:12:37 +0200

    improved version, non-interactive and all

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@
 # this script is mainly here to handle static resources
 echo "+-- handling static resources..."
 cd /id/
+
 echo "    +-- collectstatic..."
-python manage.py collectstatic
+ID_SECRET_KEY=temp python manage.py collectstatic -v0 --noinput
+
 echo "    +-- assets build..."
-python manage.py assets build
+ID_SECRET_KEY=temp python manage.py assets -v0 build
+
 echo "    +-- done."
+
 echo "+-- running the command:"
 echo "    $@"
 exec "$@"
