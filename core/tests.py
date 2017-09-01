dc3ba33261cea7cca9eae2434b1e33a587050c5e
Stas SUȘCOV <stas@nerd.ro>
2017-09-01 17:17:46 +0100

    Disable the old ticket URLs.
    Added tests for homepage and tickets homepage.

--- core/tests.py
+++ core/tests.py
@@ -1,161 +1,38 @@
-import json
-
-from django.test import TestCase
-from django.contrib.auth.models import AnonymousUser#, User
-from django.contrib.auth import get_user_model # as per https://docs.djangoproject.com/en/dev/topics/auth/customizing/#referencing-the-user-model
+from django.test import TestCase, Client
+from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
 
-from .testclient import APITestClient
 
-
-class UserTestCase(TestCase):
-    def anon_user_helper(self):
-        return AnonymousUser()
-
-    def user_helper(self, email, **kwargs):
-        return get_user_model().objects.create_user(email=email, password='top_secret', **kwargs)
+class PagesTestCase(TestCase):
 
     def setUp(self):
-        self.anonymous_user = AnonymousUser()
-        self.normal_user = self.user_helper('testuser@occrp.org')
-        self.staff_user = self.user_helper('teststaff@occrp.org', is_staff=True)
-        self.admin_user = self.user_helper('testsuperuser@occrp.org', is_superuser=True)
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            'test@email', password='top_secret')
 
-    def tearDown(self):
-        pass
+    def test_homepage(self):
+        response = self.client.get(reverse('home'))
 
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            reverse('tickets_home') + '/new',
+            response.content.decode('utf-8', 'escape')
+        )
 
-class APITestCase(TestCase):
-    def http_action(self, verb, urlname, user, urlargs={}, data={}):
-        client = APITestClient()
-        if user:
-            client.login_user(user)
-        url = reverse(urlname, kwargs=urlargs)
-        fun = getattr(client, verb)
-        if verb in ["post", "put", "delete", "patch"]:
-            res = fun(url, json.dumps(data), content_type='application/json')
-        else:
-            res = fun(url, data)
-        try:
-            res.json = json.loads(res.content)
-        except Exception as e:
-            print e
+    def test_tickets_homepage_anonymous(self):
+        response = self.client.get(reverse('tickets_home'))
 
-        return res
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse('tickets_home'), response.url)
 
-    def post(self, urlname, user=None, urlargs={}, data={}):
-        return self.http_action("post", urlname, user, urlargs, data)
+    def test_tickets_homepage_authenticated(self):
+        self.client.force_login(self.user, settings.AUTHENTICATION_BACKENDS[2])
 
-    def get(self, urlname, user=None, urlargs={}, data={}):
-        return self.http_action("get", urlname, user, urlargs, data)
+        response = self.client.get(reverse('tickets_home'))
 
-    def put(self, urlname, user=None, urlargs={}, data={}):
-        return self.http_action("put", urlname, user, urlargs, data)
-
-    def delete(self, urlname, user=None, urlargs={}, data={}):
-        return self.http_action("delete", urlname, user, urlargs, data)
-
-    def patch(self, urlname, user=None, urlargs={}, data={}):
-        return self.http_action("patch", urlname, user, urlargs, data)
-
-
-class CoreUtilsTest(TestCase):
-    def test_listchannels(self):
-        from core.models import all_known_channels
-        m = all_known_channels()
-
-
-class CoreAPIv2Test(APITestCase, UserTestCase):
-    def test_profile(self):
-        m = self.get('api_2_profile', user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn('email', m.json)
-        self.assertIn('is_admin', m.json)
-        self.assertIn('groups', m.json)
-        self.assertIn('display_name', m.json)
-        self.assertIn('notification_subscriptions', m.json)
-        self.assertIn('notifications_unseen', m.json)
-        self.assertIn('locale', m.json)
-        self.assertIn('id', m.json)
-        self.assertEqual(m.json['email'], self.normal_user.email)
-        # FIXME: need to test code path where user is a member of a network
-
-    def test_profile_bullshit_user(self):
-        m = self.get('api_2_profile')
-        self.assertEqual(m.status_code, 401)
-        self.assertIn('detail', m.json)
-
-    def test_notification_subscriptions_check_none(self):
-        # Check that (new) user isn't subscribed to anything:
-        m = self.get('api_2_notifications', user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn('notification_subscriptions', m.json)
-        self.assertEqual(m.json['notification_subscriptions'], [])
-
-    def test_notification_subscriptions_subscribe_none(self):
-        # Try subscribing to unspecified channel:
-        m = self.put('api_2_notifications', user=self.normal_user)
-        self.assertEqual(m.status_code, 400)
-        self.assertIn("error", m.json)
-
-    def test_notification_subscriptions_subscribe_erroneous(self):
-        # Try subscribing to erroneous channel:
-        m = self.put('api_2_notifications', data={'channel': 'foo'}, user=self.normal_user)
-        self.assertEqual(m.status_code, 400)
-        self.assertIn("error", m.json)
-
-    def test_notification_subscriptions_subscribe_valid_and_verify(self):
-        # Try subscribing to valid channel:
-        m = self.put('api_2_notifications', data={'channel': 'id:*:*:*:*'}, user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn("result", m.json)
-        self.assertEqual(m.json["result"], "subscribed")
-
-        # Verify subscription
-        m = self.get('api_2_notifications', user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn('notification_subscriptions', m.json)
-        self.assertEqual(m.json['notification_subscriptions'], ['id:*:*:*:*'])
-
-        # Try unsubscribing from valid channel
-        m = self.delete('api_2_notifications', data={'channel': 'id:*:*:*:*'}, user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn("found", m.json)
-        self.assertEqual(m.json["found"], 1)
-        self.assertIn("result", m.json)
-        self.assertEqual(m.json["result"], "unsubscribed")
-
-        # Double unsubscribing should result in soft fail:
-        m = self.delete('api_2_notifications', data={'channel': 'id:*:*:*:*'}, user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn("found", m.json)
-        self.assertEqual(m.json["found"], 0)
-        self.assertIn("result", m.json)
-        self.assertEqual(m.json["result"], "none")
-
-    def test_notification_subscriptions_unsubscribe_invalid(self):
-        # Try unsubscribing from invalid channel
-        m = self.delete('api_2_notifications', data={'channel': 'foo'}, user=self.normal_user)
-        self.assertEqual(m.status_code, 400)
-        self.assertIn("error", m.json)
-
-    def test_notification_subscriptions_unsubscribe_none(self):
-        # Not supplying a channel is... weird. I am a teapot!
-        m = self.delete('api_2_notifications', user=self.normal_user)
-        self.assertEqual(m.status_code, 418)
-
-    def test_notifications(self):
-        m = self.get('api_2_notifications_stream', user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-        self.assertIn('count', m.json)
-        self.assertEqual(m.json['count'], 0)
-        self.assertIn('results', m.json)
-        self.assertEqual(m.json['results'], [])
-
-        # FIXME: Notify user and test that it's working
-
-    def test_notifications_seen(self):
-        m = self.get('api_2_notifications_seen', user=self.normal_user)
-        self.assertEqual(m.status_code, 200)
-
-        # FIXME: Verify that marking notifications as seen marks them as seen
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '<div id="ember-app"></div>',
+            response.content.decode('utf-8', 'escape')
+        )
--- settings/urls.py
+++ settings/urls.py
@@ -9,8 +9,7 @@ from core.manage import Panel
 from core.admin import admin_site
 from accounts.manage import Statistics
 from core.auth import perm
-from core.views import NotificationSeen, NotificationStream
-from core.views import NotificationSubscriptions, home, tickets_home
+from core.views import home, tickets_home
 
 # Instead of admin auto-discovery:
 from databases.admin import ExternalDatabaseAdmin  # noqa
@@ -27,9 +26,6 @@ urlpatterns = [
     url(r'^about/id2/$', perm('any', TemplateView, template_name="about_id.jinja"), name='about_id'),
     url(r'^about/occrp/$', perm('any', TemplateView, template_name="about_us.jinja"), name='about_us'),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(r'^api/2/notifications/$', NotificationSubscriptions.as_view(), name='api_2_notifications'),
-    url(r'^api/2/notifications/seen/$', NotificationSeen.as_view(), name='api_2_notifications_seen'),
-    url(r'^api/2/notifications/stream/$', NotificationStream.as_view(), name='api_2_notifications_stream'),
 
     url(r'^admin/', admin_site.urls),
     url(r'^manage/$', perm('staff', Panel), name='manage_panel'),
@@ -37,10 +33,6 @@ urlpatterns = [
 
     url(r'^accounts/', include('accounts.urls')),
 
-    url(r'^notifications/seen/(?P<pk>([\d]+|all))/', perm('user', NotificationSeen), name='notification_seen'),
-    url(r'^notifications/stream/', perm('user', NotificationStream), name='notification_stream'),
-
-    url(r'^old-ticket/', include('ticket.urls')),
     url(r'^ticket/', RedirectView.as_view(
         url='/tickets/', permanent=True), name='old_ticket_redirect'),
     url(r'^databases/', include('databases.urls')),
