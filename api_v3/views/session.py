b2bfd564e8926756f100ccedbe8647875d0e1479
Stas SUȘCOV <stas@nerd.ro>
2018-03-03 04:54:36 +0000

    Fixed the session queryset error.

--- api_v3/views/session.py
+++ api_v3/views/session.py
@@ -1,11 +1,15 @@
 from rest_framework import response, viewsets
 
+from api_v3.models import Profile
 from api_v3.serializers import ProfileSerializer
 from .support import JSONApiEndpoint
 
 
 class SessionEndpoint(JSONApiEndpoint, viewsets.GenericViewSet):
     serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        return Profile.objects.none()
 
     def list(self, request, *args, **kwargs):
         serializer = self.get_serializer(request.user)
