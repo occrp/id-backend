b5699bf7b4e0b3b6741666173363c120ea424099
Stas SUȘCOV <stas@nerd.ro>
2020-02-24 11:45:14 +0000

    Silence some loggers.

--- api_v3/factories/support.py
+++ api_v3/factories/support.py
@@ -1,6 +1,12 @@
+import logging
+
 from factory import Faker, SubFactory  # noqa
 from factory.django import DjangoModelFactory, FileField  # noqa
 
+
+# Silence noisy third-party loggers...
+logging.getLogger('faker').setLevel(logging.ERROR)
+logging.getLogger('factory').setLevel(logging.ERROR)
 
 # Use a locale with unicodes
 Faker._DEFAULT_LOCALE = 'es_ES'
