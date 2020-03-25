2024e6dc6ea656c3ac7a361ba2fc301a70a5491a
Stas SUȘCOV <stas@nerd.ro>
2020-03-25 20:34:31 +0000

    Fixed the django migrations mess.

--- api_v3/migrations/0000_squashed_profile.py
+++ api_v3/migrations/0000_squashed_profile.py
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
     ]
 
     dependencies = [
-        ('auth', '__latest__'),
+        ('auth', '0008_alter_user_username_max_length'),
     ]
 
     operations = [
